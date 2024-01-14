77ee60baf5feea38c43ed84aa05f71dbcb7f381d
Kitipong Saiyot <kitisaiyot01@gmail.com>
2024-01-14 16:05:21 +0700

    update

--- Comets/functon.py
+++ Comets/functon.py
@@ -1,6 +1,13 @@
 import re
 from functools import lru_cache
 from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import ProcessPoolExecutor
+import multiprocessing
+import traceback
+import asyncio
+import concurrent.futures
+
+
 
 import pandas as pd
 from astropy.coordinates import SkyCoord
@@ -17,43 +24,43 @@ class PositionCalculator:
                         'Bennu', 'Itokawa', '-64', 'Apophis', 'Ryugu', '-122911', 'Patroclus', 
                         '90000855', 'Dinkinesh', '20065803' , 'Gaspra', 'Ceres']
 
-    @lru_cache(maxsize=None)
-    def query_horizons(self, obj, epoch):
-        query = Horizons(obj, location='@0', epochs=epoch.tdb.jd)
-        table = query.vectors(refplane='earth')
-        return table
-    def positions_normal(self, times):
-        """
-        Args:
-            times: List of times in UTC format yyyy-mm-dd hh:mm:ss
+    # @lru_cache(maxsize=None)
+    # def query_horizons(self, obj, epoch):
+    #     query = Horizons(obj, location='@0', epochs=epoch.tdb.jd)
+    #     table = query.vectors(refplane='earth')
+    #     return table
+    # def positions_normal(self, times):
+    #     """
+    #     Args:
+    #         times: List of times in UTC format yyyy-mm-dd hh:mm:ss
 
-        Returns:
-            results: DataFrame with columns ['Time', 'Object0_X', 'Object0_Y', 'Object0_Z', 'Object1_X', 'Object1_Y', ...]
-        """
-        results = []
+    #     Returns:
+    #         results: DataFrame with columns ['Time', 'Object0_X', 'Object0_Y', 'Object0_Z', 'Object1_X', 'Object1_Y', ...]
+    #     """
+    #     results = []
 
-        for time in times:
-            row = [time]
-            for obj in self.object_planet:
-                try:
-                    epoch = Time(time)
-                    table = self.query_horizons(obj, epoch)
-                    coordinates = SkyCoord(table['x'].quantity, table['y'].quantity, table['z'].quantity,
-                                           representation_type='cartesian', frame='icrs', obstime=epoch)
-                    result_str = str(coordinates)
-                    matches = re.findall(r'-?\d+\.\d+', result_str)
-                    result_list = [float(match) for match in matches]
-                except Exception as e:
-                    # print(f"Error for {obj} at time {time}: {e}")
-                    result_list = [99, 99, 99]
+    #     for time in times:
+    #         row = [time]
+    #         for obj in self.object_planet:
+    #             try:
+    #                 epoch = Time(time)
+    #                 table = self.query_horizons(obj, epoch)
+    #                 coordinates = SkyCoord(table['x'].quantity, table['y'].quantity, table['z'].quantity,
+    #                                        representation_type='cartesian', frame='icrs', obstime=epoch)
+    #                 result_str = str(coordinates)
+    #                 matches = re.findall(r'-?\d+\.\d+', result_str)
+    #                 result_list = [float(match) for match in matches]
+    #             except Exception as e:
+    #                 # print(f"Error for {obj} at time {time}: {e}")
+    #                 result_list = [99, 99, 99]
 
-                row += result_list
+    #             row += result_list
 
-            results.append(row)
+    #         results.append(row)
 
-        columns = ['Time'] + [f'{obj}_{coord}' for obj in self.object_planet for coord in ['X', 'Y', 'Z']]
-        results_df = pd.DataFrame(results, columns=columns)
-        return results_df
+    #     columns = ['Time'] + [f'{obj}_{coord}' for obj in self.object_planet for coord in ['X', 'Y', 'Z']]
+    #     results_df = pd.DataFrame(results, columns=columns)
+    #     return results_df
     
 
     def positions_parallel(self, times):
@@ -84,9 +91,52 @@ class PositionCalculator:
         columns = ['Time'] + [f'{obj}_{coord}' for obj in self.object_planet for coord in ['X', 'Y', 'Z']]
         return pd.DataFrame([row], columns=columns)
 
+class Position:
+
+    object_planet = ['SUN', '199', '299', '399', '301', '-74', '599', '699', '433', 'Eros', 'Orus', '-49',
+                     '-490', '-255', '2000016', '90000188', '90000190', '90000191', 
+                     'Leucus', 'Eurybates', 'Polymele', 'Vesta', 'Mathilde', 'Lutetia', 'Donaldjohanson', 'Braille', 'Annefrank', 
+                     'Bennu', 'Itokawa', '-64', 'Apophis', 'Ryugu', '-122911', 'Patroclus', 
+                     '90000855', 'Dinkinesh', '20065803' , 'Gaspra', 'Ceres']
+
+    async def calculate_position_single_async(self, time, obj):
+        try:
+            epoch = Time(time)
+            query = Horizons(obj, location='@0', epochs=epoch.tdb.jd)
+            table = query.vectors(refplane='earth')
+            coordinates = SkyCoord(table['x'].quantity, table['y'].quantity, table['z'].quantity,
+                                   representation_type='cartesian', frame='icrs', obstime=epoch)
+            result_str = str(coordinates)
+            matches = re.findall(r'-?\d+\.\d+', result_str)
+            result_list = [float(match) for match in matches]
+        except Exception as e:
+            # print(f"Error for {obj} at time {time}: {e}")
+            result_list = [99, 99, 99]
+
+        return obj, result_list
+
+    async def positions_parallel_async(self, times):
+        results = []
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            tasks = [self.calculate_position_single_async(time, obj) for time in times for obj in self.object_planet]
+            results = await asyncio.gather(*tasks)
+
+        data_dict = {}
+        for obj, result_list in results:
+            if obj not in data_dict:
+                data_dict[obj] = result_list
+            else:
+                data_dict[obj] += result_list
+
+        columns = ['Time'] + [f'{obj}_{coord}' for obj in self.object_planet for coord in ['X', 'Y', 'Z']]
+        rows = [[time] + data_dict[obj] for time in times for obj in self.object_planet]
+        return pd.DataFrame(rows, columns=columns)
+    
+
 
 # time_list = ['2023-12-21 15:21:42', '2023-12-22 12:00:00'] 
 # calculator = PositionCalculator()
 # result_df = calculator.calculate_positions(time_list)
 
 # result_df.to_csv('positions.csv', index=False)
+    
--- Comets/positions.py
+++ Comets/positions.py
@@ -1,4 +1,4 @@
-from functon import PositionCalculator
+from functon import PositionCalculator, Position
 import time
 
 import pandas as pd
@@ -7,6 +7,9 @@ import pandas as pd
 df0 = pd.read_csv('D:\\Project\\Earthquake\\DataSet\\new_dataset.csv')
 df0['time'] = pd.to_datetime(df0['time'])
 
+df0 = df0[(df0['time'] > '2000-01-01 00:00:00') & (df0['time'] <= '2010-01-01 00:00:00')]
+# df0 = df0[(df0['time'] > '2010-01-01 00:00:00') & (df0['time'] <= '2020-01-01 00:00:00')]
+print(len(df0))
 
 start = time.time()
 calculator = PositionCalculator()
@@ -15,4 +18,4 @@ end = time.time()
 print(result)
 print('time : ', end-start)
 
-result.to_csv('new_2000_2024_position.csv')
+result.to_csv('new_2000_2010_position.csv')
